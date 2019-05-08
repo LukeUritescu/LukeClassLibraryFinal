7180ceacef1e6e2d3967acc979e5386aa6cf8c48
Luke Uritescu <Luritescu@gmail.com>
2019-05-07 22:43:13 -0500

    Add project files.

---
+++ ClassLibraryFinal/DeliveryServices/ShippingSnail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryFinal
+{
+    public class ShippingSnail : Snail, IShippingVehicle
+    {
+        public uint TopSpeed { get; set; }
+        public uint MaxDistancePerRefuel { get; set; }
+
+        public uint MaxWeight { get; set; }
+
+        public ShippingSnail()
+        {
+            
+        }
+    }
+}
---
+++ ClassLibraryFinal/ShippingServices/DefaultShippingService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryFinal
+{
+    /// <summary>
+    /// Fake Shipping Service
+    /// </summary>
+    public class DefaultShippingService : IShippingService
+    {
+
+        public IShippingLocation ShippingLocation { get; protected set; }
+
+        public uint ShippingDistance
+        {
+            get { return getShippingDistance(); }
+        }
+
+        protected uint getShippingDistance()
+        {
+            //terrible way to determine distance insn't real
+            return (uint)Math.Abs(this.ShippingLocation.DestinationZipCode - this.ShippingLocation.StartZipCode);
+        }
+        public uint NumRefuels
+        {
+            get { return getNumRefuels(); }
+        }
+
+        private uint getNumRefuels()
+        {
+            return (uint)this.ShippingDistance / (uint)this.DeliveryService.ShippingVehicle.MaxDistancePerRefuel;
+        }
+
+        public IDeliveryService DeliveryService { get; set; }
+
+        List<IProduct> Products { get; set; }
+
+
+        /// <summary>
+        /// Used to caclulate the cost of shipping a list of products to a location using a delivery service
+        /// </summary>
+        /// <param name="Service"></param>
+        /// <param name="Products"></param>
+        /// <param name="Location"></param>
+        public DefaultShippingService(IDeliveryService Service,  List<IProduct> Products, IShippingLocation Location)
+        {
+            this.DeliveryService = Service;
+            this.Products = Products;
+            this.ShippingLocation = Location;
+            this.ShippingLocation.StartZipCode = 60605;
+            this.ShippingLocation.DestinationZipCode = 60805;
+        }
+
+        public double ShippingCost()
+        {
+            return 0;
+        }
+    }
+}
